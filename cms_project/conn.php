1f1a697734763944527242a99a90dc6a0bdc5ea0
Sam Bolgert <sbolgert@gmail.com>
2011-10-09 21:15:18 -0500

    updated credentials

--- cms_project/conn.php
+++ cms_project/conn.php
@@ -1,8 +1,8 @@
 <?php
-    define('SQL_HOST', 'hostname');
-    define('SQL_USER', 'skroot');
+    define('SQL_HOST','127.0.0.1'); 
+    define('SQL_USER', 'samb_skroot');
     define('SQL_PASS', 'Wdmd312#!');
-    define('SQL_DB', 'skroot');
+    define('SQL_DB', 'samb_skroot');
     $conn = mysql_connect(SQL_HOST,SQL_USER,SQL_PASS) or die('Could not connect to db'.mysql_error());
 
     mysql_select_db(SQL_DB, $conn) or die('Could not select db'); 

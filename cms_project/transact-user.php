0f313f19bf8dfecb94ee890a5b5a4985e4b272b1
Sam Bolgert <sbolgert@gmail.com>
2011-10-24 23:35:55 -0500

    mail system

--- cms_project/transact-user.php
+++ cms_project/transact-user.php
@@ -93,7 +93,7 @@
                     "SET email='" . $_POST['email'] .
                     "', name='" . $_POST['name'] . "', " .
                     "email_notify=".$_POST['email_notify'].
-                    "WHERE user_id=" . $_SESSION['user_id'];
+                    " WHERE user_id=" . $_SESSION['user_id'];
                 mysql_query($sql,$conn)
                     or die('Could not update your user account: ' .mysql_error());
                 }

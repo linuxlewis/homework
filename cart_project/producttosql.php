3b385be6541917e7121d009e7a02f0e3b80468ab
Sam Bolgert <sbolgert@gmail.com>
2011-11-13 15:44:23 -0600

    xml to sql

---
+++ cart_project/producttosql.php
@@ -0,0 +1,12 @@
+<?php
+    require_once 'conn.php';
+	//load XML file
+	$xml = simplexml_load_file('products.xml') or die ('Unable to load XML');
+	
+	//loop over XML elements
+	foreach ($xml as $item) {
+		$sql = "INSERT INTO store_product (product_name,product_description,product_price,product_image) VALUES ('" . addslashes($item['name']) . "','" . addslashes($item->description) . "','" . addslashes($item->price) . "','" . base64_encode(file_get_contents($item->image)) . "');";
+        mysql_query($sql,$conn) or die('query failed');
+	}
+
+?>

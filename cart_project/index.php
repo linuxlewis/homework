9d6abae3e828a1de9c65244d5a9e614e46b3b659
Sam Bolgert <sbolgert@gmail.com>
2011-11-15 14:10:56 -0600

    logout

--- cart_project/index.php
+++ cart_project/index.php
@@ -1,39 +1,67 @@
 <?php
     require_once 'conn.php';
     require_once 'header.php';
-    if(isset($_SESSION['cusomer_id'])){
+    //login 
+    if(isset($_SESSION['customer_id'])){
         //hello user
         $sql = "SELECT * from store_customer " . 
             "WHERE customer_id='" . $_SESSION['customer_id'] . "'";
         $result = mysql_query($sql, $conn) or die ('query failed');
         $userRow = mysql_fetch_array($result); 
         echo "<h3>Hello " . $userRow['customer_firstname'] . ", Welcome Back!</h3>";
-        $_SESSION['isLoggedIn'] = 1;
+        echo "<input type=\"submit\" name=\"action\" value=\"logout\" />";
     }
     else if(isset($_POST['user']) && isset($_POST['password'])){
         //login
+        $sql = "SELECT * from store_customer " . 
+            "WHERE customer_email='" . $_POST['user'] . "'";
+        $result = mysql_query($sql, $conn) or die ('query failed');
+        if(mysql_num_rows($result) > 0){
+            //user exists
+            $userRow = mysql_fetch_array($result); 
 
+            $encrypt_pw = hash("SHA256",$_POST['password']);
+
+            if($userRow['customer_password'] == $encrypt_pw){
+                $_SESSION['isLoggedIn'] = 1;
+                $_SESSION['customer_id'] = $userRow['customer_id'];
+            }
+            else{
+                echo "<h3 style=\"color:red;\">Login Error -- Please Check Credentials</h3>";
+                echo "<form method='post' action='index.php' id='login'>";
+                echo "<p>User:<input type=\"text\" name=\"user\" /></p>";
+                echo "<p>Password:<input type=\"password\" name=\"password\" /></p>";
+                echo "<p><input type=\"submit\" value=\"login\" /></p>";
+                echo "</form>";
+            }
+        }
+        else{
+            echo "<h3 style=\"color:red;\">Login Error -- Please Check Credentials</h3>";
+            echo "<form method='post' action='index.php' id='login'>";
+            echo "<p>User:<input type=\"text\" name=\"user\" /></p>";
+            echo "<p>Password:<input type=\"password\" name=\"password\" /></p>";
+            echo "<p><input type=\"submit\" value=\"login\" /></p>";
+            echo "</form>";
+        }
     }
-
+    else if(isset($_REQUEST['action'])){
+        if($_REQUEST['action'] == 'logout'){
+            unset($_SESSION['customer_id']);
+            unset($_SESSION['isLoggedIn']);
+            header('Location:index.php');
+        }
+    }
     else{
         //login form
         echo "<form method='post' action='index.php' id='login'>";
         echo "<p>User:<input type=\"text\" name=\"user\" /></p>";
         echo "<p>Password:<input type=\"password\" name=\"password\" /></p>";
+        echo "<p><input type=\"submit\" value=\"login\" /></p>";
         echo "</form>";
-
-
-
     }
-    $sessionID = session_id();
-        if(mysql_num_rows($result)==0){
-        $sql = "INSERT into store_customer(customer_id) VALUES('".$sessionID."')";
-        mysql_query($sql, $conn);
-        $_SESSION['isLoggedIn'] = 1;
-    }
-    else{
-        $_SESSION['isLoggedIn'] = 1;
-    }
+    //end login
+
+    //Start Product Page
     $sql = "SELECT * from store_product";
 
     $result = mysql_query($sql, $conn);
@@ -58,5 +86,6 @@
 
         echo "</div>";
     }
+    //end product page
     require_once 'footer.php';
 ?>

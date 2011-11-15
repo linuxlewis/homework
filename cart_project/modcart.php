5f100356197cf6d49b9abfbcf43672be436c631f
Sam Bolgert <sbolgert@gmail.com>
2011-11-15 13:00:04 -0600

    crud

--- cart_project/modcart.php
+++ cart_project/modcart.php
@@ -1,48 +1,76 @@
 <?php
     require_once 'conn.php';
     require_once 'header.php';
-    
-    switch($_POST['action']){
+    //check login
+    if($_SESSION['isLoggedIn' != 1){
+        header('Location:index.php');
+    }
+    else if(isset($_POST['cart_action'])){
+        //check action
+        switch($_POST['cart_action']){
 
         case 'add to cart':
-            $_POST['product_id']
-            $sql = "INSERT 
+            $sql = "INSERT into store_cart(customer_id,product_id) VALUES('".$_SESSION['customer_id'] .
+            "','".$_POST['product_id']."')";
+            mysql_query($sql, $conn) or die('add failed');
             break;
 
-    
+        case 'delete item':
+            $sql = "DELETE from store_cart WHERE product_id = '" . $_POST['product_id'] . "'";
+            mysql_query($sql, $conn) or die('delete failed');
+            break;
+
+        case 'empty cart':
+            $sql = "DELETE from store_cart WHERE customer_id = '" . $_SESSION['customer_id'] . "'";
+            mysql_query($sql, $conn) or die('empty failed');
+            break;
+        case 'change qty':
+            $sql = "UPDATE store_cart" . 
+            "SET cart_quantity='" . $_POST['cart_qty'] . "' " . 
+            "WHERE product_id='".$_POST['product_id'] . "' " .
+            "AND customer_id='".$_SESSION['customer_id']."'";
+
+            mysql_query($sql, $conn) or die ('update failed');
+
+        default:
+            break;
+
+        }
+
     }
-    if($_SESSION['isLoggedIn'] == 1){
+    else{
+        
+    }
+    
+    //start cart display;
 
-        $sql = "SELECT * from store_cart " .
-        " WHERE customer_id='" .session_id()."' " .
-        " LEFT OUTER JOIN store_product on store_product.product_id = store_cart.product_id ";
+    $sql = "SELECT * FROM store_cart " .
+    " LEFT OUTER JOIN store_product on store_product.product_id = store_cart.product_id " . 
+    " WHERE customer_id='" .$_SESSION['customer_id']."' ";
 
-        $result = mysql_query($sql,$conn);
+    $result = mysql_query($sql,$conn) or die('cart failed');
 
         if(mysql_num_rows($result) > 0){
+            echo """
+                <ul style=\"display:inline;\">
+                    <li>Quantity</li>
+                    <li>Image</li>
+                    <li>Name</li>
+                    <li>Price</li>
+                    <li>Extended Price</li> 
+            </ul>""";
             //iterate through rows
-            echo """<table>
-            <tr>
-                <th>Quantity</th>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Extended Price</th>
-
-            </tr>""";
             while($row = mysql_fetch_array($result)){
-                
+               echo "<tr>";
+               echo "<td><input type=\"text\" name=\"cart";
             }
 
+            echo "</table>";
         }
         else{
             echo "<h3> There are no items in your cart </h3>";
         }
-    }
-    else{
-        echo "<script>window.location.href='http://www.sambolgert.com/static_web/cart_project/index.php';</script>";
-    }
- 
+    //end cart display 
     require_once 'footer.php';
 
 ?>
